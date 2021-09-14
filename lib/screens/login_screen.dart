4492b66bdfef0f7ad97b7cb07e911aadccfb51c2
Valentina Soto <valen2298@hotmail.es>
2021-09-14 11:51:12 -0500

    Construction Email Label

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,10 +9,48 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _email = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login Screen')
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            _showLogo(),
+            _showEmail(),
+          ],
+        )
+      ),
+    );
+  }
+
+  Widget _showLogo() {
+    return Image(
+      image: AssetImage('assets/vehicles_logo.png'),
+      width: 300,
+    ); 
+  }
+
+  Widget _showEmail() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: TextField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa tu Email...',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.email),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+            ),
+        ),
+        onChanged:(value) {
+          _email = value;
+          print(_email);
+        },
       ),
     );
   }
